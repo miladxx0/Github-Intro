a430b58d62952eb815724d2788a12597234e06fb
MILADXX0 <manchesterowns@gmail.com>
2017-11-08 14:01:17 +0100

    lul

---
+++ src/Circle/mainjava.java
@@ -0,0 +1,23 @@
+package Circle;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class mainjava extends Application {
+
+	int scenewidth = 400;
+	int sceneheight = 400;
+	
+	Group root = new Group();
+	Scene scene = new Scene(root, scenewidth, sceneheight);
+	
+	
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
